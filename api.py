1bb6472f9d86d8ed4f099c83c9bcac052cec26e7
Huy Vo <huyvanvo94@gmail.com>
2018-11-01 19:17:51 -0700

    update

---
+++ api.py
@@ -0,0 +1,13 @@
+import app
+import sys
+
+
+
+if __name__ == '__main__':
+    try:
+        port = sys.argv[1]
+        print('client made at {}'.format(port))
+        app.app.run(host='localhost', port=int(port))
+
+    except:
+        print('error')
